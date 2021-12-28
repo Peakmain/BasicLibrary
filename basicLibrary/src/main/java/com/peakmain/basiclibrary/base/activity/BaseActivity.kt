e2a10b6f91ed711ed416d26eb930664ef6ab93b6
peakmain <2726449200@qq.com>
2021-12-28 13:31:12 +0800

    BaseActivity和BaseFragment的优化

--- basicLibrary/src/main/java/com/peakmain/basiclibrary/base/activity/BaseActivity.kt
+++ basicLibrary/src/main/java/com/peakmain/basiclibrary/base/activity/BaseActivity.kt
@@ -9,6 +9,7 @@ import com.peakmain.basiclibrary.BR
 import com.peakmain.basiclibrary.base.IApp
 import com.peakmain.basiclibrary.base.viewmodel.BaseViewModel
 import com.peakmain.basiclibrary.config.BasicLibraryConfig
+import java.lang.reflect.ParameterizedType
 
 /**
  * author ：Peakmain
@@ -16,30 +17,45 @@ import com.peakmain.basiclibrary.config.BasicLibraryConfig
  * mail:2726449200@qq.com
  * describe：
  */
-abstract class BaseActivity<T : ViewDataBinding, E : BaseViewModel>(private var  modelClass: Class<E>) : AppCompatActivity() {
+abstract class BaseActivity<T : ViewDataBinding, E : BaseViewModel>() :
+    AppCompatActivity() {
     abstract val layoutId: Int
     protected lateinit var mBinding: T
     protected lateinit var mViewModel: E
     private var app: IApp? = BasicLibraryConfig.getInstance().getApp()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initBefore()
         mBinding = DataBindingUtil.setContentView(this, layoutId)
-        mViewModel = getViewModel(modelClass)
+        initViewModel()
         mViewModel.initModel()
         mBinding.setVariable(BR.vm, mViewModel)
         initView()
     }
 
+    private fun initViewModel() {
+        val superClass = javaClass.genericSuperclass
+        if (superClass is ParameterizedType) {
+            val type = superClass.actualTypeArguments[1]
+            val clazz = type as Class<E>
+            mViewModel = getViewModel(clazz)
+        }else{
+            throw IllegalArgumentException("ParameterizedType error")
+        }
+
+    }
+
+
     abstract fun initView()
 
 
     fun initBefore() {
-
     }
 
     protected fun <E : ViewModel> getViewModel(modelClass: Class<E>): E {
-        if(app==null){
+        if (app == null) {
             throw NullPointerException("app must not be null")
         }
         return app!!.getViewModelProvider().get(modelClass)
