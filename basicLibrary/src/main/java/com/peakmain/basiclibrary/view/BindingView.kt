8954a2aa10bafffcc3fe9c9121cfc0531612e67d
peakmain <2726449200@qq.com>
2022-01-26 11:38:05 +0800

    BinderAdapter

--- basicLibrary/src/main/java/com/peakmain/basiclibrary/view/BindingView.kt
+++ basicLibrary/src/main/java/com/peakmain/basiclibrary/view/BindingView.kt
@@ -1,8 +1,14 @@
 package com.peakmain.basiclibrary.view
 
+import android.opengl.Visibility
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.peakmain.basiclibrary.extend.click
+import com.peakmain.basiclibrary.extend.clickClipListener
+import com.peakmain.basiclibrary.extend.clickViewDelay
+import com.peakmain.basiclibrary.extend.ktxRunOnUiThread
 import com.peakmain.ui.imageLoader.ImageLoader
 import com.peakmain.ui.utils.TextUtils
 
@@ -33,5 +39,39 @@ object BindingView {
         textView.text = spannableString
     }
 
+    /**
+     * 设置view的visibility
+     */
+    @BindingAdapter(value = ["app:visibility"])
+    @JvmStatic
+    fun setViewVisible(view: View, visibility: Int = View.VISIBLE) {
+        view.visibility = visibility
+    }
+
+    /**
+     * 防止多次重复点击
+     */
+    @BindingAdapter(value = [ "app:clickDelayTime","app:click"],requireAll = false)
+    @JvmStatic
+    fun setViewClick(view: View, delayTime: Long = 0, block: (View) -> Unit) {
+        if (delayTime == 0L) {
+            view.click {
+                block(it)
+            }
+        } else {
+            view.clickViewDelay(delayTime) {
+                block(it)
+            }
+        }
+    }
+
+    /**
+     * textView的drawableLeft和drawableRight的点击事件
+     */
+   @BindingAdapter(value = ["app:drawableLeftClick","app:drawableRightClick"],requireAll = false)
+   @JvmStatic
+   fun setTextViewDrawableClick(view:TextView,  drawableLeftClick: (View) -> Unit = {},drawableRightClick: (View) -> Unit = {}){
+       view.clickClipListener(drawableLeftClick,drawableRightClick)
+   }
 
 }
