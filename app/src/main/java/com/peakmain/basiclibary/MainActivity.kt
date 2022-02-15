18b1a1694cbfa14c2576bc805e63a6ba336f2f10
peakmain <2726449200@qq.com>
2022-02-15 16:00:21 +0800

    协程全局异常捕获

--- app/src/main/java/com/peakmain/basiclibary/MainActivity.kt
+++ app/src/main/java/com/peakmain/basiclibary/MainActivity.kt
@@ -1,23 +1,15 @@
 package com.peakmain.basiclibary
 
-import android.app.ProgressDialog
-import android.text.TextUtils
-import android.util.Log
 import android.view.View
-import android.view.ViewGroup
 import android.widget.ProgressBar
 import com.peakmain.basiclibary.databinding.ActivityMainBinding
-import com.peakmain.basiclibary.network.DataResponse
-import com.peakmain.basiclibary.network.ProjectTree
-import com.peakmain.basiclibary.network.WanAndroidApi
 import com.peakmain.basiclibary.viewModel.MainViewModel
 import com.peakmain.basiclibrary.base.activity.BaseActivity
-import com.peakmain.basiclibrary.extend.*
+import com.peakmain.basiclibrary.extend.ktxRunOnUiThreadDelay
 import com.peakmain.basiclibrary.manager.AnimationManager
-import com.peakmain.basiclibrary.network.RetrofitManager
-import com.peakmain.basiclibrary.network.status.ApiStatus
+import com.peakmain.basiclibrary.utils.GlobalCoroutineExceptionHandler
 import com.peakmain.basiclibrary.utils.bus.RxBus
-import java.util.concurrent.TimeUnit
+import com.peakmain.ui.utils.LogUtils
 
 class MainActivity(override val layoutId: Int = R.layout.activity_main) :
     BaseActivity<ActivityMainBinding, MainViewModel>() {
@@ -31,10 +23,16 @@ class MainActivity(override val layoutId: Int = R.layout.activity_main) :
         val rxBus = RxBus.instance.register<Int>("test")
         rxBus.setData(100)
         val value = rxBus.value
-        progressDialog=findViewById(R.id.progressBar)
-        findViewById<View>(R.id.tv_title).visibility=View.GONE
-        ktxRunOnUiThreadDelay(3000){
-            AnimationManager.crossfade(findViewById<View>(R.id.tv_title),progressDialog)
+        progressDialog = findViewById(R.id.progressBar)
+        findViewById<View>(R.id.tv_title).visibility = View.GONE
+        ktxRunOnUiThreadDelay(3000) {
+            AnimationManager.hideCrossFadeLoadingView(
+                findViewById<View>(R.id.tv_title),
+                progressDialog
+            )
+        }
+        GlobalCoroutineExceptionHandler().coroutineExceptionCallback = { context, exception ->
+            LogUtils.e("$context,异常是:$exception")
         }
     }
 
