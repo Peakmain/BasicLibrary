3f9fb5532c17486615d6696a8eb2e3192c58799e
peakmain <2726449200@qq.com>
2022-06-17 15:45:40 +0800

    一些问题修复

--- basicLibrary/src/main/java/com/peakmain/basiclibrary/utils/BasicLibraryUtils.kt
+++ basicLibrary/src/main/java/com/peakmain/basiclibrary/utils/BasicLibraryUtils.kt
@@ -1,5 +1,6 @@
 package com.peakmain.basiclibrary.utils
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
