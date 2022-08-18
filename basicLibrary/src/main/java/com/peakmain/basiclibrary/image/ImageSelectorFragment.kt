2c6c83c30edb920984a5412d1d8dba16f4eceadd
peakmain <chengdong.zhou@yaduo.com>
2022-08-18 18:04:52 +0800

    拍照完成

--- basicLibrary/src/main/java/com/peakmain/basiclibrary/image/ImageSelectorFragment.kt
+++ basicLibrary/src/main/java/com/peakmain/basiclibrary/image/ImageSelectorFragment.kt
@@ -1,14 +1,13 @@
 package com.peakmain.basiclibrary.image
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import com.peakmain.basiclibrary.config.ImageRequestConfig
 import com.peakmain.basiclibrary.constants.ImageSelectConstants
 import com.peakmain.basiclibrary.extend.launchImage
 import com.peakmain.basiclibrary.image.contract.SelectMultipleContract
 import com.peakmain.basiclibrary.image.contract.SelectSinglePhotoContract
+import com.peakmain.basiclibrary.image.contract.TakePictureContract
 import com.peakmain.basiclibrary.interfaces.OnImageSelectorCallback
 import com.peakmain.basiclibrary.viewmodel.ImageSelectViewModel
 
@@ -19,40 +18,47 @@ import com.peakmain.basiclibrary.viewmodel.ImageSelectViewModel
  * describe：
  */
 internal class ImageSelectorFragment : Fragment() {
-    private var mOnImageSelectorCallback: OnImageSelectorCallback?=null
+
     private var mImageSelectViewModel: ImageSelectViewModel = ImageSelectViewModel()
     private val mSelectPhotoLauncher =
         registerForActivityResult(SelectSinglePhotoContract()) { uri ->
-            mImageSelectViewModel.mStart.value = false
-            mOnImageSelectorCallback?.onImageSelect(arrayListOf(uri))
+            mImageSelectViewModel.registerSingleLauncher(uri)
         }
     private val mSelectMultiPhotoLauncher =
         registerForActivityResult(SelectMultipleContract()) { lists ->
-            mImageSelectViewModel.mStart.value = false
-            mOnImageSelectorCallback?.onImageSelect(lists)
+            mImageSelectViewModel.registerMultiLauncher(lists)
         }
 
+    private val takePictureLauncher =
+        registerForActivityResult(TakePictureContract()) {
+            mImageSelectViewModel.registerTakePicture(it)
+        }
     private val mConfig by lazy {
         arguments?.get(ImageSelectConstants.REQUEST_CONFIG) as ImageRequestConfig?
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mImageSelectViewModel.mStart.observe(this, Observer { start ->
-            mConfig?.apply {
-                if (!start) return@Observer
-                if (isSingle) {
-                    mSelectPhotoLauncher.launchImage(mConfig)
-                } else {
-                    mSelectMultiPhotoLauncher.launchImage(mConfig)
-                }
+        mImageSelectViewModel.mStart.observe(
+            this,
+            mImageSelectViewModel.imageSelectorObserver(mConfig, {
+                mSelectPhotoLauncher.launchImage(mConfig)
+            },{
+                mSelectMultiPhotoLauncher.launchImage(mConfig)
+            }) {
+                takePictureLauncher.launch(null)
             }
-        })
+        )
     }
 
 
     fun start(onImageSelectorCallback: OnImageSelectorCallback?) {
         mImageSelectViewModel.mStart.value = true
-        mOnImageSelectorCallback=onImageSelectorCallback
+        mImageSelectViewModel.mOnImageSelectorCallback = onImageSelectorCallback
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mImageSelectViewModel.clear()
     }
 }
--- basicLibrary/src/main/java/com/peakmain/basiclibrary/viewmodel/ImageSelectViewModel.kt
+++ basicLibrary/src/main/java/com/peakmain/basiclibrary/viewmodel/ImageSelectViewModel.kt
@@ -1,7 +1,12 @@
 package com.peakmain.basiclibrary.viewmodel
 
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.peakmain.basiclibrary.base.viewmodel.BaseViewModel
+import com.peakmain.basiclibrary.config.ImageRequestConfig
+import com.peakmain.basiclibrary.interfaces.OnImageSelectorCallback
 
 /**
  * author ：Peakmain
@@ -9,9 +14,50 @@ import com.peakmain.basiclibrary.base.viewmodel.BaseViewModel
  * mail:2726449200@qq.com
  * describe：
  */
-class ImageSelectViewModel : BaseViewModel() {
+internal class ImageSelectViewModel : BaseViewModel() {
     var mStart = MutableLiveData<Boolean>(false)
+    var mOnImageSelectorCallback: OnImageSelectorCallback? = null
     override fun initModel() {
 
+    }
+
+    fun clear() {
+        mOnImageSelectorCallback = null
+    }
+
+    fun registerSingleLauncher(uri: Uri?) {
+        mStart.value = false
+        mOnImageSelectorCallback?.onImageSelect(arrayListOf(uri))
+    }
+
+    fun registerMultiLauncher(lists: List<Uri?>) {
+        mStart.value = false
+        mOnImageSelectorCallback?.onImageSelect(lists)
+    }
+
+    fun registerTakePicture(pair: Pair<Boolean, Bitmap?>) {
+        if (pair.first) {
+            val bitmap = pair.second
+            mOnImageSelectorCallback?.onImageSelect(bitmap)
+        }
+    }
+
+    fun imageSelectorObserver(
+        config: ImageRequestConfig?,
+        selectPhotoLauncher: ((ImageRequestConfig) -> Unit)? = null,
+        selectMultiPhotoLauncher: ((ImageRequestConfig) -> Unit)? = null,
+        takePictureLauncher: (() -> Unit)? = null
+    )
+            : Observer<Boolean> = Observer { start ->
+        config?.also {
+            if (!start) return@Observer
+            if (config.imageType != 0 && it.isSingle) {
+                selectPhotoLauncher?.invoke(it)
+            } else if (config.imageType == 0) {
+                takePictureLauncher?.invoke()
+            } else {
+                selectMultiPhotoLauncher?.invoke(config)
+            }
+        }
     }
 }
