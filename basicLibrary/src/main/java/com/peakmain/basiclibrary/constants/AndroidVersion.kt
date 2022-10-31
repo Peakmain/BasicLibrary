bda404695badb5a7978ab7650a11c8e40cbf2d10
peakmain <chengdong.zhou@yaduo.com>
2022-10-31 14:58:34 +0800

    责任链设计模式

--- basicLibrary/src/main/java/com/peakmain/basiclibrary/constants/AndroidVersion.kt
+++ basicLibrary/src/main/java/com/peakmain/basiclibrary/constants/AndroidVersion.kt
@@ -1,6 +1,7 @@
 package com.peakmain.basiclibrary.constants
 
 import android.os.Build
+import androidx.annotation.IntDef
 
 /**
  * author ：Peakmain
@@ -11,24 +12,47 @@ import android.os.Build
 @SuppressWarnings("all")
 object AndroidVersion {
 
-    private const val ANDROID_13: Int = Build.VERSION_CODES.TIRAMISU
-    private const val ANDROID_12: Int = Build.VERSION_CODES.S
-    private const val ANDROID_11: Int = Build.VERSION_CODES.R
-    private const val ANDROID_10: Int = Build.VERSION_CODES.Q
-    private const val ANDROID_9: Int = Build.VERSION_CODES.P
-    private const val ANDROID_8_1: Int = Build.VERSION_CODES.O_MR1
-    private const val ANDROID_8: Int = Build.VERSION_CODES.O
-    private const val ANDROID_7_1: Int = Build.VERSION_CODES.N_MR1
-    private const val ANDROID_7: Int = Build.VERSION_CODES.N
-    private const val ANDROID_6: Int = Build.VERSION_CODES.M
-    private const val ANDROID_5_1: Int = Build.VERSION_CODES.LOLLIPOP_MR1
-    private const val ANDROID_5: Int = Build.VERSION_CODES.LOLLIPOP
-    private const val ANDROID_4_4_w: Int = Build.VERSION_CODES.KITKAT_WATCH
-    private const val ANDROID_4_4: Int = Build.VERSION_CODES.KITKAT
-    private const val ANDROID_4_3: Int = Build.VERSION_CODES.JELLY_BEAN_MR2
-    private const val ANDROID_4_2: Int = Build.VERSION_CODES.JELLY_BEAN_MR1
-    private const val ANDROID_4_1: Int = Build.VERSION_CODES.JELLY_BEAN
-    private const val ANDROID_4_0: Int = Build.VERSION_CODES.ICE_CREAM_SANDWICH
+    const val ANDROID_13: Int = Build.VERSION_CODES.TIRAMISU
+    const val ANDROID_12: Int = Build.VERSION_CODES.S
+    const val ANDROID_11: Int = Build.VERSION_CODES.R
+    const val ANDROID_10: Int = Build.VERSION_CODES.Q
+    const val ANDROID_9: Int = Build.VERSION_CODES.P
+    const val ANDROID_8_1: Int = Build.VERSION_CODES.O_MR1
+    const val ANDROID_8: Int = Build.VERSION_CODES.O
+    const val ANDROID_7_1: Int = Build.VERSION_CODES.N_MR1
+    const val ANDROID_7: Int = Build.VERSION_CODES.N
+    const val ANDROID_6: Int = Build.VERSION_CODES.M
+    const val ANDROID_5_1: Int = Build.VERSION_CODES.LOLLIPOP_MR1
+    const val ANDROID_5: Int = Build.VERSION_CODES.LOLLIPOP
+    const val ANDROID_4_4_w: Int = Build.VERSION_CODES.KITKAT_WATCH
+    const val ANDROID_4_4: Int = Build.VERSION_CODES.KITKAT
+    const val ANDROID_4_3: Int = Build.VERSION_CODES.JELLY_BEAN_MR2
+    const val ANDROID_4_2: Int = Build.VERSION_CODES.JELLY_BEAN_MR1
+    const val ANDROID_4_1: Int = Build.VERSION_CODES.JELLY_BEAN
+    const val ANDROID_4_0: Int = Build.VERSION_CODES.ICE_CREAM_SANDWICH
+
+    @IntDef(
+        ANDROID_13,
+        ANDROID_12,
+        ANDROID_11,
+        ANDROID_10,
+        ANDROID_9,
+        ANDROID_8_1,
+        ANDROID_8,
+        ANDROID_7_1,
+        ANDROID_7,
+        ANDROID_6,
+        ANDROID_5_1,
+        ANDROID_5,
+        ANDROID_4_4_w,
+        ANDROID_4_4,
+        ANDROID_4_3,
+        ANDROID_4_2,
+        ANDROID_4_1,
+        ANDROID_4_0
+    )
+    @Retention(AnnotationRetention.SOURCE)
+    annotation class PermissionVersion
 
     /**
      * 是否是android13及以上版本
---
+++ basicLibrary/src/main/java/com/peakmain/basiclibrary/permission/version/AndroidSPermissionVersion.kt
@@ -0,0 +1,34 @@
+package com.peakmain.basiclibrary.permission.version
+
+import android.Manifest
+import com.peakmain.basiclibrary.constants.AndroidVersion
+import com.peakmain.basiclibrary.permission.PkPermission
+
+/**
+ * author ：Peakmain
+ * createTime：2022/10/31
+ * mail:2726449200@qq.com
+ * describe：android 12 权限处理
+ */
+class AndroidSPermissionVersion :
+    IPermissionVersion {
+    override fun permissionVersion(chain: IPermissionVersion.Chain): IPermissionVersion {
+        val request = chain.request()
+        val permissionList = request.permissionList
+        if (AndroidVersion.isAndroid12()) {
+            if (permissionList.contains(Manifest.permission.ACCESS_FINE_LOCATION) &&
+                !permissionList.contains(Manifest.permission.ACCESS_COARSE_LOCATION) && !PkPermission.isGranted(
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            ) {
+                //Android 12必须添加ACCESS_COARSE_LOCATION
+                //官方适配文档：https://developer.android.google.cn/about/versions/12/approximate-location
+                throw IllegalArgumentException(
+                    "在android 12或更高的版本中，请勿单独请求ACCESS_FINE_LOCATION权限，" +
+                            "而应在单个运行时请求中同时请求ACCESS_FINE_LOCATION和ACCESS_COARSE_LOCATION权限。"
+                )
+            }
+        }
+        return chain.proceed(request)
+    }
+}
